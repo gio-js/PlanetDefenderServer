70e45db2f290653126da21eaa5bd3cf6e3881e83
Giovanni Sorgente <g.sorgente86@gmail.com>
2018-06-26 22:16:49 +0200

    config setup

--- const/app.const.service.js
+++ const/app.const.service.js
@@ -1,12 +1,12 @@
 /**
  * Default sever listening port
  */
-exports.LISTENING_PORT = 3000;
+exports.LISTENING_PORT = process.env.PORT || 3000;
 
 /**
  * Mongo DB connection
  */
-exports.MONGO_URL = 'mongodb://planetdefender:25GtAZhm@ds219051.mlab.com:19051/heroku_ft8gnss5';
+exports.MONGO_URL = process.env.MONGODB_URI;
 
 /**
  * Mongo database
